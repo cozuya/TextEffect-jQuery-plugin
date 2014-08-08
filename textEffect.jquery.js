8f9548464b993134d7a50979bb77b6f3f975e3cc
William Morriss <wjmelements@gmail.com>
2014-08-08 05:25:13 +0000

    removed all instances of self

--- textEffect.jquery.js
+++ textEffect.jquery.js
@@ -54,55 +54,52 @@ if ( typeof Object.create !== 'function' ) {
 			this.run('opacity', this.$elem.css('opacity'));
 		},
 		jumble: function () {
-			var self = this;
 			var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 			var i = 0;
 			this.setup();
 			var jumbleEffectInterval = setInterval(function () {
-				if (self.jumbleInterval) {
-					clearInterval(self.jumbleInterval);
+				if (this.jumbleInterval) {
+					clearInterval(this.jumbleInterval);
 				}
-				self.runJumble(letterArray, i);
-				self.$elem.children('span.text-effect').eq(i).html(self.oldText.substr(i, 1)).css('color', self.$elem.css('color'));
-				if (i === (self.oldText.length - 1)) {
+				this.runJumble(letterArray, i);
+				this.$elem.children('span.text-effect').eq(i).html(this.oldText.substr(i, 1)).css('color', this.$elem.css('color'));
+				if (i === (this.oldText.length - 1)) {
 					clearInterval(jumbleEffectInterval);
-					self.reset();
+					this.reset();
 				} else {
 					i++;
 				}
-			}, self.options.effectSpeed);
+			}, this.options.effectSpeed);
 		},
 		runJumble: function (letterArray, jumbleLength) {
-			var self = this;
 			this.jumbleInterval = setInterval(function () {
-				for (var i = (self.textArray.length - 1); i > jumbleLength; i--) {
-					if (self.oldText.substr(i, 1) !== ' ') {
-						self.$elem.children('span.text-effect').eq(i).html(letterArray[Math.floor(Math.random() * (letterArray.length - 1))]).css('color', self.options.jumbleColor);
+				for (var i = (this.textArray.length - 1); i > jumbleLength; i--) {
+					if (this.oldText.substr(i, 1) !== ' ') {
+						this.$elem.children('span.text-effect').eq(i).html(letterArray[Math.floor(Math.random() * (letterArray.length - 1))]).css('color', this.options.jumbleColor);
 					} else {
-						self.$elem.children('span.text-effect').eq(i).html(' ');
+						this.$elem.children('span.text-effect').eq(i).html(' ');
 					}
 				}
 			}, 70);
 		},
 		run: function (effect, oldEffect) {
-			var self = this;
 			var obj = {};
 			var i = this.options.reverse ? this.textArray.length - 1 : 0;
-			var $spans = self.$elem.children('span.text-effect');
+			var $spans = this.$elem.children('span.text-effect');
 			obj[effect] = oldEffect;
 			var effectInterval = setInterval(function () {
-				$spans.eq(i).css('visibility', 'visible').animate(obj, self.options.completionSpeed / self.textArray.length, function () {
-						if ($(this).index() === self.textArray.length - 1 && !self.options.reverse || self.options.reverse && $(this).index() === 0) {
+				$spans.eq(i).css('visibility', 'visible').animate(obj, this.options.completionSpeed / this.textArray.length, function () {
+						if ($(this).index() === this.textArray.length - 1 && !this.options.reverse || this.options.reverse && $(this).index() === 0) {
 							clearInterval(effectInterval);
-							self.reset();
+							this.reset();
 						}
 					});
-				if (self.options.reverse) {
+				if (this.options.reverse) {
 					i--;
 				} else {
 					i++;
 				}
-			}, self.options.effectSpeed);
+			}, this.options.effectSpeed);
 		},
 		reset: function () {
 			this.$elem.html(this.oldText);
